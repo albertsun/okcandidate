59d0d2808f287c0d5a359879ddb1d5393a10f540
Albert Sun <albert@albertsun.info>
2011-12-03 20:09:22 -0500

    adding data file

---
+++ r.js
@@ -0,0 +1,408 @@
+var candidates = [
+    {
+	name: "Michele Bachmann",
+	url: "http://michelebachmann.com/",
+	type: "candidate",
+	responses: [
+			{
+				question_id: "1", 
+				response_answer: "Intellegent Design", //key to question choice
+				accepted_responses: ["Intellegent Design"], //keys to question choice
+				importance: 50, 
+				explanation: "What I support is putting all science on the table and then letting students decide. I don't think it's a good idea for government to come down on one side of scientific issue or another, when there is reasonable doubt on both sides."
+			},
+			{
+				question_id: "2",
+				response_answer: "No",
+				accepted_responses: ["No"],
+				importance: 250,
+				explanation: "http://2012.republican-candidates.org/abortion.pl",
+			},
+			{
+				question_id: "3",
+				response_answer: "Maintain",
+				accepted_responses: ["Maintain"],
+				importance: 50,
+				explanation: "… it appears for President Obama that he is acting more on political strategy than military strategy. That's very concerning, because it also seems that this is more the Obama-Biden plan for early withdrawal, as opposed to the Petraeus plan.",
+			},
+			{
+				question_id: "4",
+				response_answer: "Against",
+				accepted_responses: ["Against"],
+				importance: 250,
+				explanation: "I\'m running for the presidency of the United States. And I don\'t see that it\'s the role of a president to go into states and interfere with their state laws. I do support a constitutional amendment on marriage between a man and a woman, but I would not be going into the states to overturn their state law, Source: 2011 GOP primary debate in Manchester NH Jun 13, 2011",
+			},
+			{
+				question_id: "5",
+				response_answer: "Yes",
+				accepted_responses: ["Yes"],
+				importance: 250,
+				explanation: "We agree with Jimmy that the rent is too damn high",
+			}
+			]
+		},
+    {
+	name: "Newt Gingrich",
+	url: "http://newt.org/",
+	type: "candidate",
+	responses: [
+			{
+				question_id: "1", 
+				response_answer: "Yes", 
+				accepted_responses: ["Yes", "Intellegent Design"],
+				importance: 1, 
+				explanation: "Evolution certainly seems to express the closest understanding we can now have….I believe evolution should be taught as science, and intelligent design should be taught as philosophy."
+			},
+			{
+				question_id: "2",
+				response_answer: "No",
+				accepted_responses: ["No"],
+				importance: 10,
+				explanation: "http://2012.republican-candidates.org/abortion.pl",
+			},
+			{
+				question_id: "3",
+				response_answer: "Maintain",
+				accepted_responses: ["Maintain"],
+				importance: 50,
+				explanation: "None of the generals recommended the speed of the drawdown that the president wants",
+			},
+			{
+				question_id: "4",
+				response_answer: "Against",
+				accepted_responses: ["Against"],
+				importance: 10,
+				explanation: "\“I believe that marriage is between a man and woman,\” Gingrich said, the Des Moines Register reports. \“It has been for all of recorded history and I think this is a temporary aberration that will dissipate. I think that it is just fundamentally goes against everything we know.\” (http://2012.talkingpointsmemo.com/2011/09/gingrich-gay-marriage-is-a-temporary-aberration-that-will-dissipate.php)",
+			},
+			{
+				question_id: "5",
+				response_answer: "Yes",
+				accepted_responses: ["Yes"],
+				importance: 250,
+				explanation: "We agree with Jimmy that the rent is too damn high",
+			}
+			]
+		},
+	{
+	name: "Jon Huntsman",
+	url: "http://jon2012.org/",
+	type: "candidate",
+	responses: [
+			{
+				question_id: "1", 
+				response_answer: "Yes", 
+				accepted_responses: ["Yes"], 
+				importance: 50, 
+				explanation: "I believe in evolution and trust scientists on global warming. Call me crazy."
+			},
+			{
+				question_id: "2",
+				response_answer: "No",
+				accepted_responses: ["No"],
+				importance: 50,
+				explanation: "http://2012.republican-candidates.org/abortion.pl",
+			},
+			{
+				question_id: "3",
+				response_answer: "Lower",
+				accepted_responses: ["Lower"],
+				importance: 50,
+				explanation: "If you can't define a winning exit strategy for the American people, where we somehow come out ahead, then we're wasting our money, and we're wasting our strategic resources … It's a tribal state, and it always will be. Whether we like it or not, whenever we withdraw from Afghanistan, whether it's now or years from now, we'll have an incendiary situation. Should we stay and play traffic cop? I don't think that serves our strategic interests.",
+			},
+			{
+				question_id: "4",
+				response_answer: "Civil Unions",
+				accepted_responses: ["Civil Unions","Against"],
+				importance: 10,
+				explanation: "I believe in civil unions. I think we can do a better job in this country as it relates to equality and basic reciprocal rights, (http://thehill.com/video/campaign/178941-huntsman-i-believe-in-civil-unions)",
+			},
+			{
+				question_id: "5",
+				response_answer: "Yes",
+				accepted_responses: ["Yes"],
+				importance: 250,
+				explanation: "We agree with Jimmy that the rent is too damn high",
+			}			
+			]
+		},
+	{
+	name: "Gary Johnson",
+	url: "http://garyjohnson2012.com/",
+	type: "candidate",
+	responses: [
+			{
+			question_id: "1", //key to question
+			response_answer: "Yes", //key to question choice
+			accepted_responses: ["Yes"], //keys to question choice
+			importance: 10, //one of 0, 1, 10, 50, 250
+			explanation: "Lorem ipsum..."
+			},
+			{
+				question_id: "2",
+				response_answer: "Yes",
+				accepted_responses: ["Yes","No"],
+				importance: 1,
+				explanation: "http://2012.republican-candidates.org/abortion.pl",
+			},
+			{
+				question_id: "3",
+				response_answer: "Lower",
+				accepted_responses: ["Lower"],
+				importance: 50,
+				explanation: "",
+			},
+			{
+				question_id: "4",
+				response_answer: "For",
+				accepted_responses: ["For"],
+				importance: 10,
+				explanation: "For a very long time, society has viewed gay marriage as a moral and, yes, religious issue. Today, I believe we have arrived at a point in history where more and more Americans are viewing it as a question of liberty and freedom. That evolution is important, and the time has come for us to align our marriage laws with the notion that every individual should be treated equally (http://reason.com/blog/2011/12/02/gary-johnson-comes-out-in-favor-of-gay-m)",
+			},
+			{
+				question_id: "5",
+				response_answer: "Yes",
+				accepted_responses: ["Yes"],
+				importance: 250,
+				explanation: "We agree with Jimmy that the rent is too damn high",
+			}	
+			]
+		},
+	{
+	name: "Jimmy McMillan",
+	url: "http://mcmillan2012.com/",
+	type: "candidate",
+	responses: [
+			{
+			question_id: "5", //key to question
+			response_answer: "Yes", //key to question choice
+			accepted_responses: ["Yes"], //keys to question choice
+			importance: 250, //one of 0, 1, 10, 50, 250
+			explanation: "The rent is too damn high!"
+			}
+			]
+		},
+		{
+	name: "Ron Paul",
+	url: "http://ronpaul2012.com/",
+	type: "candidate",
+	responses: [
+			{
+			question_id: "1", //key to question
+			response_answer: "No", //key to question choice
+			accepted_responses: ["1", "2", "3"], //keys to question choice
+			importance: 50, //one of 0, 1, 10, 50, 250
+			explanation: "I think there is a theory, and I don't accept it."
+			},
+			{
+				question_id: "2",
+				response_answer: "No",
+				accepted_responses: ["No"],
+				importance: 10,
+				explanation: "http://2012.republican-candidates.org/abortion.pl",
+			},
+			{
+				question_id: "3",
+				response_answer: "Lower",
+				accepted_responses: ["Lower"],
+				importance: 250,
+				explanation: "",
+			},
+			{
+				question_id: "4",
+				response_answer: "Against",
+				accepted_responses: ["Against"],
+				importance: 50,
+				explanation: "",
+			},
+			{
+				question_id: "5",
+				response_answer: "Yes",
+				accepted_responses: ["Yes"],
+				importance: 250,
+				explanation: "We agree with Jimmy that the rent is too damn high",
+			}	
+			
+			]
+		},
+		{
+	name: "Rick Perry",
+	url: "http://rickperry.org/",
+	type: "candidate",
+	responses: [
+			{
+			question_id: "1", //key to question
+			response_answer: "Intelligent Design", //key to question choice
+			accepted_responses: ["Intelligent Design"], //keys to question choice
+			importance: 50, //one of 0, 1, 10, 50, 250
+			explanation: ""
+			},
+			{
+				question_id: "2",
+				response_answer: "No",
+				accepted_responses: ["No"],
+				importance: 50,
+				explanation: "http://2012.republican-candidates.org/abortion.pl",
+			},
+			{
+				question_id: "3",
+				response_answer: "Maintain",
+				accepted_responses: ["Maintain"],
+				importance: 50,
+				explanation: "",
+			},
+			{
+				question_id: "4",
+				response_answer: "Against",
+				accepted_responses: ["Against"],
+				importance: 50,
+				explanation: "",
+			},
+			{
+				question_id: "5",
+				response_answer: "Yes",
+				accepted_responses: ["Yes"],
+				importance: 250,
+				explanation: "We agree with Jimmy that the rent is too damn high",
+			}	
+			]
+		},
+		{
+	name: "Mitt Romney",
+	url: "http://MittRomney.com/",
+	type: "candidate",
+	responses: [
+			{
+			question_id: "1", //key to question
+			response_answer: "Yes", //key to question choice
+			accepted_responses: ["Intelligent Design", "Yes"], //keys to question choice
+			importance: 1, //one of 0, 1, 10, 50, 250
+			explanation: "I\'m not exactly sure what is meant by intelligent design, he said. \"But I believe God is intelligent, and I believe he designed the creation. And I believe he used the process of evolution to create the human body."
+			},
+			{
+				question_id: "2",
+				response_answer: "No",
+				accepted_responses: ["No"],
+				importance: 10,
+				explanation: "http://2012.republican-candidates.org/abortion.pl",
+			},
+			{
+				question_id: "3",
+				response_answer: "Maintain",
+				accepted_responses: ["Maintain"],
+				importance: 50,
+				explanation: "",
+			},
+			{
+				question_id: "4",
+				response_answer: "Against",
+				accepted_responses: ["Against"],
+				importance: 50,
+				explanation: "",
+			},
+			{
+				question_id: "5",
+				response_answer: "No",
+				accepted_responses: ["No"],
+				importance: 50,
+				explanation: "",
+			}	
+			]
+		},	
+		{
+	name: "Rick Santorum",
+	url: "http://RickSantorum.com/",
+	type: "candidate",
+	responses: [
+			{
+			question_id: "1", //key to question
+			response_answer: "No", //key to question choice
+			accepted_responses: ["No"], //keys to question choice
+			importance: 250, //one of 0, 1, 10, 50, 250
+			explanation: "I believe in Genesis 1:1"
+			},
+			{
+				question_id: "2",
+				response_answer: "No",
+				accepted_responses: ["No"],
+				importance: 250,
+				explanation: "http://2012.republican-candidates.org/abortion.pl",
+			},
+			{
+				question_id: "3",
+				response_answer: "Maintain",
+				accepted_responses: ["Maintain"],
+				importance: 50,
+				explanation: "",
+			},
+			{
+				question_id: "4",
+				response_answer: "Against",
+				accepted_responses: ["Against"],
+				importance: 250,
+				explanation: "You know, the gay community and those who support gay rights with respect to marriage in particular -- no, I am not going to find an accommodation with that",
+			},
+			{
+				question_id: "5",
+				response_answer: "Yes",
+				accepted_responses: ["Yes"],
+				importance: 250,
+				explanation: "We agree with Jimmy that the rent is too damn high",
+			}
+			]
+		},			
+		]
+
+		
+		
+		
+		
+		
+		
+		var questions = [
+    {
+	id: "1",
+	question: "Do you believe in the Evolution",
+	desc: "Charles Darwin's theory of evolution, not the bad movie where they defeated ailens with shampoo",
+	choices: [
+	    {answer: "Yes", desc: "I believe in evolution"},
+	    {answer: "No", desc: "I do not Believe in evolution"},
+	    {answer: "Intellegent Design", desc: "I support the theory of intelligent design"}
+	]
+    },
+    {
+	id: "2",
+	question: "Do you support a woman's right to choose",
+	desc: "Make no mistake, this is the abortion question.",
+	choices: [
+	    {answer: "Yes", desc: "Yes, I am Pro Choice"},
+	    {answer: "No", desc: "No, I am Pro Life"}
+	]
+    },
+    {
+	id: "3",
+	question: "Should we Maintain or Lower presence in afganistan",
+	choices: [
+	    {answer: "Maintain", desc: "I want to maintain current troop levels"},
+	    {answer: "Lower", desc: "I want to lower current troop levels"}
+	]
+    },
+	{
+	id: "4",
+	question: "What is your stance on Same-Sex Marriage",
+	choices: [
+	    {answer: "For", desc: "I am for gay marriage"},
+	    {answer: "Against", desc: "I am against gay marriage"},
+	    {answer: "Civil Unions", desc: "I am for civil unions"},
+		
+	]
+    },
+	{
+	id: "5",
+	question: "Is the rent too damn high",
+	choices: [
+	    {answer: "Yes", desc: "The Rent is too damn high"},
+	    {answer: "No", desc: "The Rent is not too damn high"}
+		
+	]
+    }
+];
